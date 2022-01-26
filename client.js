f40300762d22f92f7885430bc7413895702927e6
Brant McCarthy <brant.mccarthy@me.com>
2022-01-25 23:39:42 -0600

    finished base goal

--- client.js
+++ client.js
@@ -31,13 +31,23 @@ const employees = [
   }
 ];
 
+// YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
+
+// This problem is massive! Break the problem down, take small steps, and test as you go.
+// What is the fewest lines of code I can write and test to get just a little closer?
+
+// This is not a race. Everyone on your team should understand what is happening.
+// Ask questions when you don't.
+
 let employeeBonusArray = [];
 
 function bonusCalc(inputArray, outputArray){
   for (let i = 0; i < inputArray.length; i++){
     let ratingPercentage = 0;
+    let salary = Number(inputArray[i].annualSalary); // To avoid string concat later
+    
+    // Establish Rating Percentages
     if (inputArray[i].reviewRating < 3){
-      finalPercentage = 0;
       ratingPercentage = 0;
     }
     if (inputArray[i].reviewRating === 3){
@@ -49,77 +59,34 @@ function bonusCalc(inputArray, outputArray){
     if (inputArray[i].reviewRating === 5){
       ratingPercentage = 0.10;
     }
+
+    // +5% for employees w/ 15yr tenure (determined by employeeNumber)
     if (inputArray[i].employeeNumber < 10000){
       ratingPercentage += 0.05;
     }
-    if (inputArray[i].annualSalary > 65000){
-      ratingPercentage -= 0.01;
+
+    // -1% for employees w/ 65k+ Salary
+    if (salary > 65000){
+      ratingPercentage -= 0.01;      
     }
+
+    // Enforce 0% Min, 13% Max Bonus
     if (ratingPercentage < 0){
       ratingPercentage = 0;
     }
     if (ratingPercentage > 0.13){
       ratingPercentage = 0.13;
     }
-    // console.log('annual salary:', Number(inputArray[i].annualSalary));
-    // console.log('bonus:', inputArray[i].annualSalary * ratingPercentage);
-    
+
+    // Push final values to array
     outputArray[i] = {
       name: inputArray[i].name,
       bonusPercentage: ratingPercentage,
-      totalCompensation: Number(inputArray[i].annualSalary + (inputArray[i].annualSalary * ratingPercentage)),
-      totalBonus: Number(inputArray[i].annualSalary * ratingPercentage),
+      totalCompensation: salary + (salary * ratingPercentage),
+      totalBonus: salary * ratingPercentage,
     }
-  }
-}
+  } // end for
+} // end bonusCalc
 
 bonusCalc(employees, employeeBonusArray);
 console.log(employeeBonusArray);
-
-// function bonusCalc() {
-//   reviewCalc();
-//   // employeeNumberCalc(array);
-// }
-
-// function reviewCalc(){
-//   let ratingPercentage = 0;
-//   let finalBonus = 0;
-//   for (let employee of employees){
-//     let bonus = {
-//       name:
-//     }
-//     if (employee.reviewRating < 3){
-//       ratingPercentage = 0;
-//       bonus. = {
-//         annualSalary * ratingPercentage,
-//       }
-//     }
-//     if (employee.reviewRating === 3 ){
-//       ratingPercentage = 0.04;
-//       employee = {
-//         bonusAmount: employee.annualSalary * ratingPercentage,
-//       }
-//     }
-//     if (employee.reviewRating === 4 ){
-//       ratingPercentage = 0.06;
-//       employee = {
-//         bonusAmount: employee.annualSalary * ratingPercentage,
-//       }
-//     }
-//     if (employee.reviewRating === 5 ){
-//       ratingPercentage = 0.10;
-//       employee = {
-//         bonusAmount: employee.annualSalary * ratingPercentage,
-//       }
-//     }
-//   }
-// }
-
-
-// YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
-
-// This problem is massive! Break the problem down, take small steps, and test as you go.
-// What is the fewest lines of code I can write and test to get just a little closer?
-
-// This is not a race. Everyone on your team should understand what is happening.
-// Ask questions when you don't.
